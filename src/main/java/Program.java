12b97154c7ebe0b3034a7a30674287892381520d
markeloff86 <omg5746923>
2017-10-05 11:03:00 +0600

    initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Program {
+    public static void main(String[] args) {
+        JFrame mainFrame=new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
